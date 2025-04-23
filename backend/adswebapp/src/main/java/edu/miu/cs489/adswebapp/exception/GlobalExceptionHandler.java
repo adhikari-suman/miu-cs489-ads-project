92507c073df3088699ba2af44def53082cd8a95c
Suman Adhikari <expertxkid@gmail.com>
2025-04-23 13:57:27 -0500

    feat/CS489::Project: populate errors if any in GlobalExceptionHandler

--- backend/adswebapp/src/main/java/edu/miu/cs489/adswebapp/exception/GlobalExceptionHandler.java
+++ backend/adswebapp/src/main/java/edu/miu/cs489/adswebapp/exception/GlobalExceptionHandler.java
@@ -4,8 +4,12 @@ import edu.miu.cs489.adswebapp.exception.patient.DuplicatePatientFoundException;
 import edu.miu.cs489.adswebapp.exception.patient.PatientNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,7 +19,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiError> handlePatientNotFoundException(PatientNotFoundException e) {
         ApiError apiError = new ApiError(
                 HttpStatus.NOT_FOUND.value(),
-                e.getMessage()
+                e.getMessage(), null
         );
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiError);
@@ -25,7 +29,24 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiError> handleDuplicatePatientFoundException(DuplicatePatientFoundException e) {
         ApiError apiError = new ApiError(
                 HttpStatus.BAD_REQUEST.value(),
-                e.getMessage()
+                e.getMessage(), null
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        Map<String, String> validationErrors = new HashMap<>();
+
+        e.getBindingResult().getFieldErrors().forEach(error -> {
+            validationErrors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        ApiError apiError = new ApiError(
+                HttpStatus.BAD_REQUEST.value(),
+                "Validation failed",
+                validationErrors
         );
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
