8e8c16a596a645e444728e58a4ab9ad098f06b3d
Suman Adhikari <expertxkid@gmail.com>
2025-04-23 09:58:19 -0500

    feat/CS489::Project: update home component

--- frontend/ads-react-app/app/home/home.tsx
+++ frontend/ads-react-app/app/home/home.tsx
@@ -1,3 +1,14 @@
+import { ProtectedRoute } from "~/core/components/protected-route";
+import { useAuth } from "~/core/hooks/useAuth";
+
 export default function Home(){
-    return <h1>Hello world!</h1>;
+    
+    const isLoggedIn = useAuth();
+
+
+    return (
+    <ProtectedRoute>
+        <h1>Hello world!</h1>
+    </ProtectedRoute>
+    );
 }
