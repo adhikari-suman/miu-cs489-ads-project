461418b3a19b1f2557a77b95f267f2843c95f6f6
Suman Adhikari <expertxkid@gmail.com>
2025-04-23 10:01:49 -0500

    feat/CS489::Project: add ProtectedRoute, Login and Register component

--- frontend/ads-react-app/app/routes.ts
+++ frontend/ads-react-app/app/routes.ts
@@ -1,3 +1,7 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("home/home.tsx")] satisfies RouteConfig;
+export default [
+    index("home/home.tsx"),
+    route("/login", "auth/login.tsx"),
+    route("/register", "auth/register.tsx"),
+] satisfies RouteConfig;
