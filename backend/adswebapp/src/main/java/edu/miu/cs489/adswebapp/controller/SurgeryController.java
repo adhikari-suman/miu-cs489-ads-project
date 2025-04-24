7e7aaf0ecf00bfa16b3ef0390664aec9fba0fe0d
Suman Adhikari <expertxkid@gmail.com>
2025-04-23 22:20:37 -0500

    refactor/CS489::Project: implement all methods of SurgeryController

--- backend/adswebapp/src/main/java/edu/miu/cs489/adswebapp/controller/SurgeryController.java
+++ backend/adswebapp/src/main/java/edu/miu/cs489/adswebapp/controller/SurgeryController.java
@@ -1,41 +1,47 @@
 package edu.miu.cs489.adswebapp.controller;
 
+import edu.miu.cs489.adswebapp.dto.response.AppointmentResponseDTO;
+import edu.miu.cs489.adswebapp.dto.response.SurgeryResponseDTO;
+import edu.miu.cs489.adswebapp.service.AppointmentService;
+import edu.miu.cs489.adswebapp.service.SurgeryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/surgeries")
 @RequiredArgsConstructor
 public class SurgeryController {
-    @GetMapping
-    public ResponseEntity<List<Object>> getSurgeries(
+    private final SurgeryService     surgeryService;
+    private final AppointmentService appointmentService;
 
-                                                    ) {
-        return ResponseEntity.ok(
-                List.of("Surgery 1", "Surgery 2", "Surgery 3", "Surgery 4", "Surgery 5")
-                                );
+    @GetMapping
+    public ResponseEntity<Page<SurgeryResponseDTO>> getSurgeries(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "surgeryNo") String sortBy,
+            @RequestParam(defaultValue = "asc") String order
+                                                                ) {
+        return ResponseEntity.ok(surgeryService.getAllSurgeries(page, size, sortBy, order));
     }
 
     @GetMapping("/{surgeryNo}")
-    public ResponseEntity<Object> getSurgeryBySurgeryNo(
+    public ResponseEntity<SurgeryResponseDTO> getSurgeryBySurgeryNo(
             @PathVariable("surgeryNo") String surgeryNo
-                                                   ) {
-        return ResponseEntity.ok(
-                "Surgery with surgery no: " + surgeryNo + " found."
-                                );
+                                                                   ) {
+        return ResponseEntity.ok(surgeryService.getSurgeryBySurgeryNo(surgeryNo));
     }
 
     @GetMapping("/{surgeryNo}/appointments")
-    public ResponseEntity<List<Object>> getAppointmentsForSurgeryBySurgeryNo(
-            @PathVariable("surgeryNo") String surgeryNo
-                                                       ) {
+    public ResponseEntity<Page<AppointmentResponseDTO>> getAppointmentsForSurgeryBySurgeryNo(
+            @PathVariable("surgeryNo") String surgeryNo,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "appointmentId") String sortBy,
+            @RequestParam(defaultValue = "asc") String order
+                                                                                            ) {
         return ResponseEntity.ok(
-                List.of("Appointment 1", "Appointment 2", "Appointment 3", "Appointment 4", "Appointment 5")
-                                );
+                appointmentService.getAppointmentsForSurgeryBySurgeryNo(surgeryNo, page, size, sortBy, order));
     }
-
-
 }
