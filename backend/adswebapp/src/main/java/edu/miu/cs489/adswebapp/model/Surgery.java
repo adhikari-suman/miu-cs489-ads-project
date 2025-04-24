695a5f62a5b69048df896e0ae0ddbad45601907c
Suman Adhikari <expertxkid@gmail.com>
2025-04-23 22:19:33 -0500

    refactor/CS489::Project: add a prefix for Surgery

--- backend/adswebapp/src/main/java/edu/miu/cs489/adswebapp/model/Surgery.java
+++ backend/adswebapp/src/main/java/edu/miu/cs489/adswebapp/model/Surgery.java
@@ -14,6 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Surgery {
+    public static final String SURGERY_ID_PREFIX = "SURG-";
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
