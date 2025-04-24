08a4d62dfbc72811e50dd0296eccdc857abca2c4
Suman Adhikari <expertxkid@gmail.com>
2025-04-23 21:10:51 -0500

    refactor/CS489::Project: add dentistId to DentistResponseDTO

--- backend/adswebapp/src/main/java/edu/miu/cs489/adswebapp/dto/response/DentistResponseDTO.java
+++ backend/adswebapp/src/main/java/edu/miu/cs489/adswebapp/dto/response/DentistResponseDTO.java
@@ -1,5 +1,4 @@
 package edu.miu.cs489.adswebapp.dto.response;
 
-public record DentistResponseDTO(Integer id, String firstName, String lastName, String specialization) {
-
+public record DentistResponseDTO(String dentistId, String firstName, String lastName, String specialization) {
 }
