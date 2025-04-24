0efcb469f50ea22c82db5a9acf95d185a40836e8
Suman Adhikari <expertxkid@gmail.com>
2025-04-23 19:51:04 -0500

    feat/CS489::Project: add getAllAppointments in AppointmentController

--- backend/adswebapp/src/main/java/edu/miu/cs489/adswebapp/controller/AppointmentController.java
+++ backend/adswebapp/src/main/java/edu/miu/cs489/adswebapp/controller/AppointmentController.java
@@ -1,5 +1,9 @@
 package edu.miu.cs489.adswebapp.controller;
 
+import edu.miu.cs489.adswebapp.dto.response.AppointmentResponseDTO;
+import edu.miu.cs489.adswebapp.service.AppointmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,10 +11,22 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/appointments")
+@RequiredArgsConstructor
 public class AppointmentController {
+    private final AppointmentService appointmentService;
+
+
+
     @GetMapping
-    public ResponseEntity<List<Object>> getAllAppointments() {
-        return ResponseEntity.ok(List.of());
+    public ResponseEntity<Page<AppointmentResponseDTO>> getAllAppointments(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "appointmentDateTime") String sortBy,
+            @RequestParam(defaultValue = "asc") String order
+                                                          ) {
+        Page<AppointmentResponseDTO> appointmentResponseDTOs = appointmentService.getAppointments(page, size, sortBy,
+                                                                                                order);
+        return ResponseEntity.ok(appointmentResponseDTOs);
     }
 
     @GetMapping("/{appointmentId}")
