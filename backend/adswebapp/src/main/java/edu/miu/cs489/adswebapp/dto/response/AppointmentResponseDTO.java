340abf8f52fe7dafcc1b2889d88bd29026cb48b6
Suman Adhikari <expertxkid@gmail.com>
2025-04-23 21:10:33 -0500

    refactor/CS489::Project: change appointmentDateTime to LocalDateTime in AppointmentResponseDTO

--- backend/adswebapp/src/main/java/edu/miu/cs489/adswebapp/dto/response/AppointmentResponseDTO.java
+++ backend/adswebapp/src/main/java/edu/miu/cs489/adswebapp/dto/response/AppointmentResponseDTO.java
@@ -3,8 +3,9 @@ package edu.miu.cs489.adswebapp.dto.response;
 import edu.miu.cs489.adswebapp.model.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public record AppointmentResponseDTO(String appointmentId, PatientResponseDTO patient, DentistResponseDTO dentist,
-                                     SurgeryResponseDTO surgery, LocalDate appointmentDateTime,
+                                     SurgeryResponseDTO surgery, LocalDateTime appointmentDateTime,
                                      AppointmentStatus appointmentStatus, BillResponseDTO bill) {
 }
