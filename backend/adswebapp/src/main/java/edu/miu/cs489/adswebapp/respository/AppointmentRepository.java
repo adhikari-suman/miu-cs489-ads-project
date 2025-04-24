2376299d0ad179e6896166d44830ce3b8cc4f6f6
Suman Adhikari <expertxkid@gmail.com>
2025-04-23 22:19:12 -0500

    refactor/CS489::Project: add methods to Surgery and Appointment repository

--- backend/adswebapp/src/main/java/edu/miu/cs489/adswebapp/respository/AppointmentRepository.java
+++ backend/adswebapp/src/main/java/edu/miu/cs489/adswebapp/respository/AppointmentRepository.java
@@ -1,7 +1,11 @@
 package edu.miu.cs489.adswebapp.respository;
 
+import aj.org.objectweb.asm.commons.Remapper;
 import edu.miu.cs489.adswebapp.model.Appointment;
 import edu.miu.cs489.adswebapp.model.AppointmentStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
@@ -13,5 +17,7 @@ public interface AppointmentRepository extends JpaRepository<Appointment, Intege
 
     int countByDentistIdAndAppointmentStatusInAndAppointmentDateTimeBetween(Integer dentistId,
                                                                          AppointmentStatus[] statuses, LocalDateTime startDate, LocalDateTime endDate);
+
+    Page<Appointment> findAllBySurgeryId(Integer surgeryId, Pageable pageable);
 
 }
--- backend/adswebapp/src/main/java/edu/miu/cs489/adswebapp/respository/SurgeryRepository.java
+++ backend/adswebapp/src/main/java/edu/miu/cs489/adswebapp/respository/SurgeryRepository.java
@@ -3,5 +3,9 @@ package edu.miu.cs489.adswebapp.respository;
 import edu.miu.cs489.adswebapp.model.Surgery;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface SurgeryRepository extends JpaRepository<Surgery, Integer> {
+    Optional<Surgery> findBySurgeryNo(String surgeryNo);
 }
