d099928a9e9f68c3cbfa2286e8c067f8ef2793ba
Suman Adhikari <expertxkid@gmail.com>
2025-04-23 19:50:44 -0500

    feat/CS489::Project: add and implement getAppointments() in AppointmentService

--- backend/adswebapp/src/main/java/edu/miu/cs489/adswebapp/service/AppointmentService.java
+++ backend/adswebapp/src/main/java/edu/miu/cs489/adswebapp/service/AppointmentService.java
@@ -1,6 +1,9 @@
 package edu.miu.cs489.adswebapp.service;
 
+import edu.miu.cs489.adswebapp.dto.response.AppointmentResponseDTO;
 import edu.miu.cs489.adswebapp.model.Appointment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
 
@@ -8,4 +11,11 @@ public interface AppointmentService {
     Appointment addAppointment(Appointment appointment);
     List<Appointment> getAllAppointments();
 
+
+    Page<AppointmentResponseDTO> getAppointments(
+            int page,
+            int size,
+            String sortBy,
+            String sortDirection
+                                                );
 }
--- backend/adswebapp/src/main/java/edu/miu/cs489/adswebapp/service/impl/AppointServiceImpl.java
+++ backend/adswebapp/src/main/java/edu/miu/cs489/adswebapp/service/impl/AppointServiceImpl.java
@@ -1,11 +1,16 @@
 package edu.miu.cs489.adswebapp.service.impl;
 
 
+import edu.miu.cs489.adswebapp.dto.response.AppointmentResponseDTO;
+import edu.miu.cs489.adswebapp.mapper.AppointmentMapper;
 import edu.miu.cs489.adswebapp.model.Appointment;
 import edu.miu.cs489.adswebapp.respository.AppointmentRepository;
 import edu.miu.cs489.adswebapp.service.AppointmentService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,6 +21,7 @@ import java.util.List;
 public class AppointServiceImpl implements AppointmentService {
 
     private final AppointmentRepository appointmentRepository;
+    private final AppointmentMapper     appointmentMapper;
 
 
     @Override
@@ -26,5 +32,12 @@ public class AppointServiceImpl implements AppointmentService {
     @Override
     public List<Appointment> getAllAppointments() {
         return appointmentRepository.findAll();
+    }
+
+    @Override
+    public Page<AppointmentResponseDTO> getAppointments(int page, int size, String sortBy, String sortDirection) {
+        PageRequest pageRequest = PageRequest.of(page, size, Sort.by(Sort.Direction.fromString(sortDirection), sortBy));
+
+        return appointmentRepository.findAll(pageRequest).map(appointmentMapper::appointmentToAppointmentResponseDTO);
     }
 }
