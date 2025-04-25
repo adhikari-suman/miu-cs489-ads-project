9aac88ebf095274ce6c73e432684d188db97c06f
Suman Adhikari <expertxkid@gmail.com>
2025-04-25 05:39:08 -0500

    refactor/CS489::Project: remove unused imports

--- backend/adswebapp/src/main/java/edu/miu/cs489/adswebapp/AdswebappApplication.java
+++ backend/adswebapp/src/main/java/edu/miu/cs489/adswebapp/AdswebappApplication.java
@@ -1,20 +1,8 @@
 package edu.miu.cs489.adswebapp;
 
-import edu.miu.cs489.adswebapp.model.Address;
-import edu.miu.cs489.adswebapp.model.Patient;
-import edu.miu.cs489.adswebapp.respository.PatientRepository;
-import edu.miu.cs489.adswebapp.service.PatientService;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.sql.SQLOutput;
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.ArrayList;
-import java.util.Date;
 
 @SpringBootApplication
 @RequiredArgsConstructor
